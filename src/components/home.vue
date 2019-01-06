b9be07cec7d38848912bdba55073d45cb0452edc
hqc <zyc@sina.com>
2019-01-06 09:23:12 +0800

    home完成-退出-验证

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,159 @@
 <template>
-    <div>Home</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">
+            <img src="@/assets/logo.png" alt>
+          </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1" class="loginout">
+          <a href="#" @click.prevent="handleLoginout()">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">
+        <el-menu
+          unique-opened
+          default-active="2"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-success"></i>
+              用户列表
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- ----------------------------------------------------- -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+
+            <el-menu-item index="2-1">
+              <i class="el-icon-success"></i>
+              角色列表
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-success"></i>
+              权限列表
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- ----------------------------------------------------- -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+
+            <el-menu-item index="3-1">
+              <i class="el-icon-success"></i>
+              商品列表
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <i class="el-icon-success"></i>
+              分类参数
+            </el-menu-item>
+            <el-menu-item index="3-3">
+              <i class="el-icon-success"></i>
+              商品分类
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- ----------------------------------------------------- -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+
+            <el-menu-item index="4-1">
+              <i class="el-icon-success"></i>
+              订单列表
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- ----------------------------------------------------- -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+
+            <el-menu-item index="5-1">
+              <i class="el-icon-success"></i>
+              数据报表
+            </el-menu-item>
+          </el-submenu>
+
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
+  // 如果token就渲染home
+  // 反之就不渲染--->回到登录页面
+  beforeCreate () {
+    if (!localStorage.getItem('token')) {
+      this.$message.warning('请先登录')
+      this.$router.push({
+        name: 'login'
+      })
+    }
+  },
+  methods: {
+    handleLoginout () {
+      // 1.清除token
+      localStorage.clear()
+      // 2.回到登录组件
+      this.$router.push({
+        name: 'login'
+      })
+      // 3.提示
+      this.$message.success('退出成功')
+    }
+  }
 }
 </script>
 
 <style>
+.container {
+  height: 100%;
+}
 
+.container .header {
+  background-color: #b3c0d1;
+}
+
+.container .aside {
+}
+
+.container .main {
+}
+
+.middle {
+  text-align: center;
+  line-height: 60px;
+  color: white;
+}
+
+.loginout a {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="wrap">
-        <el-form class="form" label-position="top" label-width="80px" :model="formdata">
-          <h2>用户登录</h2>
-          <el-form-item label="用户名">
-            <el-input v-model="formdata.username">  </el-input>
-          </el-form-item>
-          <el-form-item label="密码">
-            <el-input v-model="formdata.password"></el-input>
-          </el-form-item>
-          <el-button @click="handleLogin()" type="primary" class="btn">登录</el-button>
-        </el-form>
-    </div>
+  <div class="wrap">
+    <el-form class="form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username">  </el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button @click="handleLogin()" type="primary" class="btn">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -24,21 +24,25 @@ export default {
     }
   },
   methods: {
-    handleLogin () {
-      this.$http
-        .post('login', this.formdata)
-        .then((res) => {
-        console.log(res)
+    async handleLogin () {
+      const res = await this.$http.post('login', this.formdata)
+      // console.log(res);
 
-          const {data: {meta: {msg, status}}} = res
-          if (status === 200) {
-            this.$router.push({
-              name: "home"
-            })
-          } else {      
-            this.$message.warning(msg)
-          }
+      const {
+        data: {
+          meta: {msg, status},
+          data
+        }
+      } = res
+      if (status === 200) {
+        // 在登录成功时保存用户的token值（token将来会用到)
+        localStorage.setItem('token', data.token)
+        this.$router.push({
+          name: 'home'
         })
+      } else {
+        this.$message.warning(msg)
+      }
     }
   },
   mounted () {
@@ -67,3 +71,17 @@ export default {
         width: 100%;
     }
 </style>
+
+// .then((res) => {
+//          console.log(res)
+//           const {data: {
+//               meta: {msg, status}
+//               }} = res
+//           if (status === 200) {
+//             this.$router.push({
+//               name: "home"
+//             })
+//           } else {
+//             this.$message.warning(msg)
+//           }
+//         })
